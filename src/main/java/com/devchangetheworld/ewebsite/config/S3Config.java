49a870f483a381d774eb11aeb450290ddda51cd7
BuiChienThang <107343290+ThangBuiChien@users.noreply.github.com>
2024-10-07 20:17:18 +0700

    Remove connected with key, use IAM center instead

--- src/main/java/com/devchangetheworld/ewebsite/config/S3Config.java
+++ src/main/java/com/devchangetheworld/ewebsite/config/S3Config.java
@@ -3,6 +3,7 @@ package com.devchangetheworld.ewebsite.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,21 +12,32 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${aws.access.key.id}")
-    private String accessKeyId;
-
-    @Value("${aws.secret.access.key}")
-    private String secretAccessKey;
+//    @Value("${aws.access.key.id}")
+//    private String accessKeyId;
+//
+//    @Value("${aws.secret.access.key}")
+//    private String secretAccessKey;
+//
+//    @Value("${aws.s3.region}")
+//    private String region;
+//
+//    @Bean
+//    public AmazonS3 s3Client() {
+//        AWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+//        return AmazonS3ClientBuilder.standard()
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .withRegion(region)
+//                .build();
+//    }
 
     @Value("${aws.s3.region}")
     private String region;
 
     @Bean
     public AmazonS3 s3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
         return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
                 .build();
     }
 }
